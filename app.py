ea5a1d2f64660139d7915bfccd2ac707daf798da
Jose Eloy Ramos <joseeloy@MacBook-Air-de-Jose-Eloy.local>
2024-09-25 21:00:04 +0200

    Añadir hola {nombre} y configuración de Flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from os import name
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
-def hello(): return 'Hola, mundo!'
+def saludo():
+    return render_template('index.html', nombre='Jose Eloy')
 
-if name == 'main': 
-    app.run(debug=True) > app.py
+if __name__ == '__main__':
+    app.run(debug=True)
